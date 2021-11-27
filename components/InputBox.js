546c7cf6fd009350e50175789b6a80c63e255b5d
Addisu87 <addisuhaile87@gmail.com>
2021-11-27 19:48:09 +0300

    Widgets edited

--- components/InputBox.js
+++ components/InputBox.js
@@ -76,24 +76,24 @@ function InputBox() {
 	};
 
 	return (
-		<div className='bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6'>
-			<div className='flex space-x-4 p-4 items-center'>
+		<div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
+			<div className="flex space-x-4 p-4 items-center">
 				<Image
-					className='rounded-full'
+					className="rounded-full"
 					src={session.user.image}
-					width='40'
-					height='40'
-					layout='fixed'
-					alt=''
+					width="40"
+					height="40"
+					layout="fixed"
+					alt=""
 				/>
-				<form className='flex flex-1'>
+				<form className="flex flex-1">
 					<input
-						className='rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none'
-						type='text'
+						className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+						type="text"
 						ref={inputRef}
 						placeholder={`What's on your mind, ${session.user.name}?`}
 					/>
-					<button hidden type='submit' onClick={sendPost}>
+					<button hidden type="submit" onClick={sendPost}>
 						Submit
 					</button>
 				</form>
@@ -101,38 +101,50 @@ function InputBox() {
 				{imageToPost && (
 					<div
 						onClick={removeImage}
-						className='flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer'
+						className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
 					>
-						<img className='h-10 object-contain' src={imageToPost} alt='' />
-						<p className='text-xs text-red-500 text-center'>Remove</p>
+						<Image
+							className="h-10 object-contain"
+							src={imageToPost}
+							alt=""
+						/>
+						<p className="text-xs text-red-500 text-center">
+							Remove
+						</p>
 					</div>
 				)}
 			</div>
 
-			<div className='flex justify-evenly p-3 border-t'>
-				<div className='inputIcon'>
-					<VideoCameraIcon className='h-7 text-red-500' />
-					<p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
+			<div className="flex justify-evenly p-3 border-t">
+				<div className="inputIcon">
+					<VideoCameraIcon className="h-7 text-red-500" />
+					<p className="text-xs sm:text-sm xl:text-base">
+						Live Video
+					</p>
 				</div>
 
 				<div
 					onClick={() => filepickerRef.current.click()}
-					className='inputIcon'
+					className="inputIcon"
 				>
-					<CameraIcon className='h-7 text-green-400' />
-					<p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+					<CameraIcon className="h-7 text-green-400" />
+					<p className="text-xs sm:text-sm xl:text-base">
+						Photo/Video
+					</p>
 					<input
 						ref={filepickerRef}
 						onChange={addImageToPost}
-						type='file'
+						type="file"
 						hidden
 					/>
 				</div>
 
-				<div className='inputIcon'>
-					<EmojiHappyIcon className='h-7 text-yellow-300' />
+				<div className="inputIcon">
+					<EmojiHappyIcon className="h-7 text-yellow-300" />
 				</div>
-				<p className='text-xs sm:text-sm xl:text-base'>Feeling/Activity</p>
+				<p className="text-xs sm:text-sm xl:text-base">
+					Feeling/Activity
+				</p>
 			</div>
 		</div>
 	);
--- components/Login.js
+++ components/Login.js
@@ -3,16 +3,17 @@ import { signIn } from 'next-auth/client';
 
 function Login() {
 	return (
-		<div className='grid place-items-center'>
+		<div className="grid place-items-center">
 			<Image
-				src='https://links.papareact.com/t4i'
+				src="https://links.papareact.com/t4i"
 				height={400}
 				width={400}
-				objectFit='contain'
+				objectFit="contain"
+				alt=""
 			/>
 			<h1
 				onClick={signIn}
-				className='p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer'
+				className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer"
 			>
 				Login with Facebook
 			</h1>
